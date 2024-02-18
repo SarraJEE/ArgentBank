0388f36bfa418098065ea2a621e13d1802471d24
SarraJEE <bgrsarra@gmail.com>
2024-02-18 13:10:46 +0100

    nclure la gestion de l'authentification dans les routes.

--- front/src/App.js
+++ front/src/App.js
@@ -1,11 +1,22 @@
 import * as React from "react";
-import { Routes, Route, BrowserRouter } from "react-router-dom";
+import { Routes, Route, BrowserRouter, Navigate } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import Login from "./pages/Login/Login";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
 import User from "./pages/User/User";
 import Error from "./pages/Error/Error";
+import { useSelector } from "react-redux";
+
+const PrivateRouteComponent = ({ element, ...props }) => {
+  const isAuth = useSelector((state) => state.login.isAuth);
+
+  return isAuth ? (
+    React.cloneElement(element, props) // Utiliser React.cloneElement pour transmettre les props
+  ) : (
+    <Navigate to="/login" replace />
+  );
+};
 
 const App = () => {
   return (
@@ -14,7 +25,10 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/profile" element={<User />} />
+        <Route
+          path="/profile"
+          element={<PrivateRouteComponent element={<User />} />}
+        />
         <Route path="*" element={<Error />} />
       </Routes>
       <Footer />
--- front/src/components/EditName/EditName.jsx
+++ front/src/components/EditName/EditName.jsx
@@ -1,67 +1,112 @@
-import React, { useState, useEffect } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import { updateProfile } from '../../redux/services/ApiService';
+import React, { useState, useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { updateProfile } from "../../redux/services/ApiService";
 
 const EditName = ({ onEdit }) => {
-    const firstName = useSelector(state => state.user.firstName);
-    const lastName = useSelector(state => state.user.lastName);
-    const userName = useSelector(state => state.user.userName);
-    const [edit, showEdit] = useState(false);
-    const dispatch = useDispatch();
-    const token = useSelector(state => state.login.token);
-    const [newUserName, setNewUserName] = useState('');
+  const firstName = useSelector((state) => state.user.firstName);
+  const lastName = useSelector((state) => state.user.lastName);
+  const userName = useSelector((state) => state.user.userName);
+  const [edit, showEdit] = useState(false);
+  const dispatch = useDispatch();
+  const token = useSelector((state) => state.login.token);
+  const [newUserName, setNewUserName] = useState("");
 
-    const submit = (e) => {
-        e.preventDefault();
-        dispatch(updateProfile(newUserName, token));
-        setNewUserName('');
-        showEdit(false);
-        onEdit(false);
+  const submit = (e) => {
+    e.preventDefault();
+    dispatch(updateProfile(newUserName, token));
+    setNewUserName("");
+    showEdit(false);
+    onEdit(false);
+  };
 
-    };
+  useEffect(() => {
+    setNewUserName(userName);
+  }, [userName]);
 
-    useEffect(() => {
-        setNewUserName(userName);
-    }, [userName]);
+  return (
+    <div className="header">
+      <h1 className={`header ${edit ? "editing" : ""}`}>
+        {edit ? (
+          "Edit user info"
+        ) : (
+          <>
+            Welcome back <br /> {firstName} {lastName}
+          </>
+        )}
+      </h1>
 
-    return (
-        <div className="header">
-            <h1 className={`header ${edit ? 'editing' : ''}`}>
-                {edit ? 'Edit user info' : <>Welcome back <br /> {firstName} {lastName}</>}
-            </h1>
-
-            {
-                edit ?
-                    <form className='edit-inputs-buttons' onSubmit={submit}>
-                        <div className='edit-inputs'>
-                            <div className='edit-input-group'>
-                                <label>User name :</label>
-                                <input
-                                    className='edit-input'
-                                    onChange={(e) => { setNewUserName(e.target.value) }}
-                                    value={newUserName}
-                                    required
-                                />
-                            </div>
-                            <div className='edit-input-group'>
-                                <label>First name :</label>
-                                <input type="text" className='edit-input' value={firstName} disabled />
-                            </div>
-                            <div className='edit-input-group'>
-                                <label>Last name :</label>
-                                <input type="text" className='edit-input' value={lastName} disabled />
-                            </div>
-                        </div>
-                        <div className='edit-buttons'>
-                            <button className='edit-button-option' type='submit'>Save</button>
-                            <button className='edit-button-option' onClick={() => { showEdit(false); onEdit(false); }}>Cancel</button>
-                        </div>
-                    </form>
-                    :
-                    <button className="edit-button" onClick={() => { showEdit(true); onEdit(true); }}>Edit Name</button>
-            }
-        </div>
-    )
-}
+      {edit ? (
+        <form className="edit-inputs-buttons" onSubmit={submit}>
+          <div className="edit-inputs">
+            <div className="edit-input-group">
+              <label for="user name ">
+                User name :
+              </label>
+              <input
+                 type="text"
+                 id="user name"
+                 name="user name"
+                className="edit-input"
+                onChange={(e) => {
+                  setNewUserName(e.target.value);
+                }}
+                value={newUserName}
+                required
+              />
+            </div>
+            <div className="edit-input-group">
+              <label  for="First name">
+                First name :
+              </label>
+              <input
+                type="text"
+                id="First name"
+                className="edit-input"
+                value={firstName}
+                disabled
+              />
+            </div>
+            <div className="edit-input-group">
+              <label for="Last name">
+                Last name :
+              </label>
+              <input
+                type="text"
+                id="Last name"
+                className="edit-input"
+                value={lastName}
+                disabled
+              />
+            </div>
+          </div>
+          <div className="edit-buttons">
+            <button className="edit-button-option" type="submit">
+              Save
+            </button>
+            <button
+              className="edit-button-option"
+              onClick={() => {
+                showEdit(false);
+                onEdit(false);
+              }}
+            >
+              Cancel
+            </button>
+          </div>
+        </form>
+      ) : (
+        <button
+          className="edit-button"
+          onClick={() => {
+            showEdit(true);
+            onEdit(true);
+          }}
+        >
+          Edit Name
+        </button>
+      )}
+    </div>
+  );
+};
 
 export default EditName;
